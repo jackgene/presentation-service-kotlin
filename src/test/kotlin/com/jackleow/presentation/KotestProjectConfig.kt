ee81097b9d2c30c8d01fde59eecd9e847d974f49
Jack Leow <jleow@tubi.tv>
2023-08-31 15:07:55 -0700

    Parallelize tests.

--- src/test/kotlin/com/jackleow/presentation/KotestProjectConfig.kt
+++ src/test/kotlin/com/jackleow/presentation/KotestProjectConfig.kt
@@ -1,9 +1,12 @@
 package com.jackleow.presentation
 
 import io.kotest.core.config.AbstractProjectConfig
+import java.lang.Integer.max
 
 object KotestProjectConfig : AbstractProjectConfig() {
     init {
         displayFullTestPath = true
     }
+
+    override val parallelism = max(2, Runtime.getRuntime().availableProcessors() * 4 / 3)
 }
