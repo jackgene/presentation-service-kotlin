70c244f754257ecd53704a1939402dfad675a2f7
Jack Leow <jackgene@gmail.com>
2023-08-31 15:08:59 -0700

    Added tokenizer property-based tests.

---
+++ src/test/kotlin/com/jackleow/presentation/flows/tokenizing/MappedKeywordsTokenizerProp.kt
@@ -0,0 +1,45 @@
+package com.jackleow.presentation.flows.tokenizing
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.collections.shouldContainAll
+import io.kotest.property.Arb
+import io.kotest.property.Gen
+import io.kotest.property.arbitrary.map
+import io.kotest.property.arbitrary.string
+import io.kotest.property.arbitrary.stringPattern
+import io.kotest.property.checkAll
+
+class MappedKeywordsTokenizerProp : WordSpec({
+    "A MappedKeywordsTokenizer" should {
+        val keywordsByRawToken: Gen<Map<String, String>> =
+            Arb.map(Arb.stringPattern("[a-z]+"), Arb.string(), 1)
+
+        "extract all mapped tokens" {
+            checkAll(keywordsByRawToken) { keywordsByRawToken: Map<String, String> ->
+                // Set up
+                val instance = MappedKeywordsTokenizer(keywordsByRawToken)
+
+                // Test
+                val actualTokens: List<String> = instance(keywordsByRawToken.keys.joinToString(" "))
+
+                // Verify
+                assert(actualTokens.toSet() == keywordsByRawToken.values.toSet())
+            }
+        }
+
+        "only extract mapped tokens" {
+            checkAll(
+                keywordsByRawToken, Arb.string()
+            ) { keywordsByRawToken: Map<String, String>, text: String ->
+                // Set up
+                val instance = MappedKeywordsTokenizer(keywordsByRawToken)
+
+                // Test
+                val actualTokens: List<String> = instance("$text ${keywordsByRawToken.keys.first()}")
+
+                // Verify
+                keywordsByRawToken.values.toSet() shouldContainAll actualTokens.toSet()
+            }
+        }
+    }
+})
---
+++ src/test/kotlin/com/jackleow/presentation/flows/tokenizing/NormalizedWordsTokenizerProp.kt
@@ -0,0 +1,85 @@
+package com.jackleow.presentation.flows.tokenizing
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.collections.shouldNotContain
+import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
+import io.kotest.matchers.ints.shouldBeLessThanOrEqual
+import io.kotest.matchers.should
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.positiveInt
+import io.kotest.property.arbitrary.set
+import io.kotest.property.arbitrary.string
+import io.kotest.property.arbitrary.stringPattern
+import io.kotest.property.checkAll
+
+class NormalizedWordsTokenizerProp : WordSpec({
+    "A NormalizedWordsTokenizer" should {
+        "only extract hyphenated lower-case tokens" {
+            checkAll(Arb.string()) { text: String ->
+                // Set up
+                val instance = NormalizedWordsTokenizer()
+
+                // Test
+                val actualTokens: List<String> = instance(text)
+
+                // Verify
+                for (actualToken: String in actualTokens) {
+                    for (c: Char in actualToken) {
+                        c should { it == '-' || it.isLowerCase() }
+                    }
+                }
+            }
+        }
+
+        "never extract stop words" {
+            checkAll(
+                Arb.set(Arb.stringPattern("[a-z]+"), 1..100), Arb.string()
+            ) { stopWords: Set<String>, text: String ->
+                // Set up
+                val instance = NormalizedWordsTokenizer(stopWords)
+
+                // Test
+                val actualTokens: List<String> = instance(text)
+
+                // Verify
+                for (actualToken: String in actualTokens) {
+                    stopWords shouldNotContain actualToken
+                }
+            }
+        }
+
+        "only extract words longer than minWordLength" {
+            checkAll(
+                Arb.positiveInt(), Arb.string()
+            ) { minWordLength: Int, text: String ->
+                // Set up
+                val instance = NormalizedWordsTokenizer(minWordLength = minWordLength)
+
+                // Test
+                val actualTokens: List<String> = instance(text)
+
+                // Verify
+                for (actualToken: String in actualTokens) {
+                    actualToken.length shouldBeGreaterThanOrEqual minWordLength
+                }
+            }
+        }
+
+        "only extract words shorter than maxWordLength" {
+            checkAll(
+                Arb.positiveInt(), Arb.string()
+            ) { maxWordLength: Int, text: String ->
+                // Set up
+                val instance = NormalizedWordsTokenizer(maxWordLength = maxWordLength)
+
+                // Test
+                val actualTokens: List<String> = instance(text)
+
+                // Verify
+                for (actualToken: String in actualTokens) {
+                    actualToken.length shouldBeLessThanOrEqual maxWordLength
+                }
+            }
+        }
+    }
+})
