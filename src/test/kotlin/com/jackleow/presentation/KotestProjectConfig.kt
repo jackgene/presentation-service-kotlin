576e3d55be2894a021eb390c25a524f2b4dad993
Jack Leow <jackgene@gmail.com>
2023-09-06 09:04:36 -0700

    Verify GitHub Actions test parallelism.

--- src/test/kotlin/com/jackleow/presentation/KotestProjectConfig.kt
+++ src/test/kotlin/com/jackleow/presentation/KotestProjectConfig.kt
@@ -1,12 +1,16 @@
 package com.jackleow.presentation
 
 import io.kotest.core.config.AbstractProjectConfig
-import java.lang.Integer.max
 
 object KotestProjectConfig : AbstractProjectConfig() {
     init {
         displayFullTestPath = true
     }
 
-    override val parallelism = max(2, Runtime.getRuntime().availableProcessors() * 4 / 3)
+    override val parallelism = run {
+        val par = Runtime.getRuntime().availableProcessors()
+        println("Parallelism: $par")
+
+        par
+    }
 }
